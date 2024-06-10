9f20d80656114660625424b60d956e5ec9e88dd3
ohziiilov3 <klbaskerville0520@gmail.com>
2024-06-09 22:32:05 -0700

    chore: add select, option and style

--- frontend/kudos-board-frontend/src/components/NewSpaceForm/NewSpaceForm.jsx
+++ frontend/kudos-board-frontend/src/components/NewSpaceForm/NewSpaceForm.jsx
@@ -7,6 +7,15 @@ const NewSpaceForm = () => {
     <div className="form-container">
       <h2>Create a Hi-Five</h2>
       <input type="text" placeholder="Title" />
+      <input type="text" placeholder="Author" />
+      <select name="" id="">
+       <option value="">Select a category</option>
+       <option value="">All</option>
+       <option value="">Recent</option>
+       <option value="">Encouragement</option>
+       <option value="">Thank you</option>
+       <option value="">Inspiration</option>
+     </select>
       <textarea placeholder="Message" rows="4"></textarea>
       <button type="submit">Submit</button>
     </div>
