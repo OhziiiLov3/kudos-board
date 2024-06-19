06bdcac334343378b77ca77cf92ab3bb776adbf5
ohziiilov3 <klbaskerville0520@gmail.com>
2024-06-18 23:44:27 -0700

    add: setup server, config prisma and migrate models

--- frontend/kudos-board-frontend/src/components/SpacePage/SpacePage.jsx
+++ frontend/kudos-board-frontend/src/components/SpacePage/SpacePage.jsx
@@ -1,11 +1,24 @@
-import '../SpacePage/SpacePage.css'
+import Header from "../Header/Header";
+import "../SpacePage/SpacePage.css";
+import { FaArrowLeft } from "react-icons/fa";
+import {Link} from 'react-router-dom'
 
 const SpacePage = () => {
   return (
     <div>
-      <h1>Space Page</h1>
+      <div className="space-card-container">
+        <Link to='/' className="back-link">
+      <FaArrowLeft />
+        </Link>
+        <Header />
+        <div className="create-card-container">
+          <button className="create-card-btn filter-btn">
+            Create a Card
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SpacePage
+export default SpacePage;
