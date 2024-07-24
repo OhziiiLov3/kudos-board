f09c89ce642a35d621726bcbfb02d064e74d780c
ohziiilov3 <klbaskerville0520@gmail.com>
2024-07-23 18:52:51 -0700

    Update API URL to use deployed backend

--- frontend/kudos-board-frontend/src/services/CardApi.js
+++ frontend/kudos-board-frontend/src/services/CardApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {getToken} from '../services/UserApi';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL ;
 
 
 export const createCard = async (cardData) => {
